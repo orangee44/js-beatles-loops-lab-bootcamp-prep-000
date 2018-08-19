d4881eed450e18e2f3c065ee35bd3962d38c7656
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 19:48:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// add solution here
+function theBeatlesPlay() {
+  
+}
+
+
+
+  1) Beatles Loops returns an array of strings containing what instruments eachmusician plays:
+     ReferenceError: theBeatlesPlay is not defined
+      at Context.<anonymous> (test/index-test.js:6:5)
